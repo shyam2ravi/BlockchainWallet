fe1860535ab643b23023af9aa42cc0b27abd4508
shyam2ravi <shyam2ravi@gmail.com>
2023-09-29 23:30:46 +0530

    Update main.go
    Signature verification Pool

--- goblockchain/main.go
+++ goblockchain/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"goblockchain/block"
 	"goblockchain/wallet"
 	"log"
 )
@@ -11,11 +12,24 @@ func init() {
 }
 
 func main() {
-	w := wallet.NewWallet()
-	fmt.Println("Private Key: ", w.PrivateKeyStr())
-	fmt.Println("Public Key: ", w.PublicKeyStr())
-	fmt.Println("Blockchain Address: ", w.BlockchainAddress())
+	walletM := wallet.NewWallet()
+	walletA := wallet.NewWallet()
+	walletB := wallet.NewWallet()
 
-	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
-	fmt.Printf("signature: %s\n", t.GenerateSignature())
+	// Wallet
+	t := wallet.NewTransaction(walletA.PrivateKey(), walletA.PublicKey(), walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0)
+
+	// Blockchain
+	blockchain := block.NewBlockchain(walletM.BlockchainAddress())
+	isAdded := blockchain.AddTransaction(walletA.BlockchainAddress(), walletB.BlockchainAddress(), 1.0,
+		walletA.PublicKey(), t.GenerateSignature())
+	fmt.Println("Added? ", isAdded)
+
+	blockchain.Mining()
+	blockchain.Print()
+
+	fmt.Printf("A %.1f\n", blockchain.CalculateTotalAmount(walletA.BlockchainAddress()))
+	fmt.Printf("B %.1f\n", blockchain.CalculateTotalAmount(walletB.BlockchainAddress()))
+	fmt.Printf("M %.1f\n", blockchain.CalculateTotalAmount(walletM.BlockchainAddress()))
+
 }
