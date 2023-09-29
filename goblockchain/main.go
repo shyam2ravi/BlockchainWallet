1137d910fca6101a119abbd6f286e8af9a377400
shyam2ravi <shyam2ravi@gmail.com>
2023-09-29 23:07:28 +0530

    Update main.go
    Print Signature

--- goblockchain/main.go
+++ goblockchain/main.go
@@ -12,7 +12,10 @@ func init() {
 
 func main() {
 	w := wallet.NewWallet()
-	fmt.Println(w.PrivateKeyStr())
-	fmt.Println(w.PublicKeyStr())
-	fmt.Println(w.BlockchainAddress())
+	fmt.Println("Private Key: ", w.PrivateKeyStr())
+	fmt.Println("Public Key: ", w.PublicKeyStr())
+	fmt.Println("Blockchain Address: ", w.BlockchainAddress())
+
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("signature: %s\n", t.GenerateSignature())
 }
